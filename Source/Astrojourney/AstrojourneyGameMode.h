502a828c399859f56f9d48e8b9c3e9aeb640c6d0
Karlo Sadural <karlo.sadural@gmail.com>
2024-06-23 10:36:53 -0400

    Initial push
    Placing uproject files into GitHub

---
+++ Source/Astrojourney/AstrojourneyGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AstrojourneyGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAstrojourneyGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAstrojourneyGameMode();
+};
+
+
+
